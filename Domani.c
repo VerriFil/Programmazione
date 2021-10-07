f53c71fffb9230641a801dddf14e80e868570778
VerriFil <91255636+VerriFil@users.noreply.github.com>
2021-10-07 11:35:25 +0200

    Create Domani.c

---
+++ Domani.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+
+    int anno=0;
+    printf("Inserisci anno: ");
+    scanf("%d", &anno);
+
+    if(anno%4==0){
+        if(anno%100==0)
+            printf("\nL'anno non e' bisestile\n");
+        else
+            printf("\nL'anno e' bisestile\n");
+    }
+    else
+        printf("\nL'anno non e' bisestile\n");
+
+    return 0;
+}
