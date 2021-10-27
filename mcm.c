23bde8f4d8b898042e3463e305c2021e74b7f25a
VerriFil <91255636+VerriFil@users.noreply.github.com>
2021-10-27 11:58:12 +0200

    Create mcm.c

---
+++ mcm.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(){
+
+    int a=0, b=0, mcm=0, j; //j==box
+
+    printf("Inserisci il primo numero primo: ");
+    scanf("%d", &a);
+    printf("\nInserisci il secondo numero primo: ");
+    scanf("%d", &b);
+
+    if(a>b){
+        j=a;
+	    while(j%b!=0){
+		    j+=a;
+	    }
+	    printf("\n\nIl mcm e' %d\n", j);
+    }
+    else{
+        j=b;
+	    while(j%a!=0){
+		    j+=b;
+	    }
+	    printf("\n\nIl mcm e' %d\n", j);
+    }
+
+    return 0;
+}
