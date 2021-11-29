9a069e40b206a3b2ea4504c50b1573e3eea4a00d
VerriFil <91255636+VerriFil@users.noreply.github.com>
2021-11-29 16:35:39 +0100

    Create tut8es1.c

---
+++ tut8es1.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int occorrenza(int *a, int dim, int *elmPiuRipetuto){
+	int i,j;
+	int cnt, cnmax=0, valRip;
+	
+	for(i=0; i<dim; i++){
+		cnt=0;
+		//secondo for che controlla ogni elemento dell'array alla volta
+		for(j=0;j<dim;j++){ 
+			if(a[i]==a[j]){ //se il valore nella cella si ripete aumenta il cnt
+				cnt++;
+			}
+		}
+		
+		if(cnt>cnmax){ //se l'occorrenza massima è più piccola di cnt questa cambia
+			cnmax=cnt;
+			valRip=a[i]; //e il valore riputo si aggiorna
+		}
+	}
+	
+	*elmPiuRipetuto=valRip;
+	return cnmax;
+}
+
+int main(){
+	int n, k, i;
+	int rip, contatore;
+	
+	printf("Inserire un numero: ");
+	scanf("%d", &n);
+	printf("\nInserire un'altro numero: ");
+	scanf("%d", &k);
+	
+	int numeri[n];
+	srand(time(NULL));
+	for(i=0;i<n;i++)
+		numeri[i]=rand()%k;
+		
+	occorrenza(numeri, k, &rip);
+	
+	contatore=occorrenza(numeri, n, &rip);
+	
+	printf("\n");
+	for(i=0;i<n;i++)
+		printf("%d ", numeri[i]);
+			
+	printf("\nIl numero %d e' il piu' ripetuto, con ben %d ripetizioni", rip, contatore);
+	
+	return 0;
+}
